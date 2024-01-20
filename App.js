55f8971e659762c65c70b29e67060123edeff7d4
jamiema1 <jkmm.1024@gmail.com>
2024-01-20 15:18:33 -0800

    Created folder structure

--- App.js
+++ App.js
@@ -1,31 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native'
+import { GlobalStyles } from './styles/globalStyles';
+import Homepage from './screens/homepage/homepage';
 
 export default function App() {
+
+  console.log(styles.container)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Image
-        style={styles.logo}
-        source={require('./assets/notifire_logo.png')}
-      />
-      <StatusBar style="auto" />
+        <Homepage />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    ...GlobalStyles.globalStyles,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-
-  logo: {
-    borderRadius: 25,
-    width: 75,
-    height: 75,
-  },
+  }
 });
