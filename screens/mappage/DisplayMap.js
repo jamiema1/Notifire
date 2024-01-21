7ed54c022ce8a1ed10ca3d10aab8693027e025e0
jamiema1 <jkmm.1024@gmail.com>
2024-01-20 21:02:46 -0800

    Added markers to map

--- screens/mappage/DisplayMap.js
+++ screens/mappage/DisplayMap.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
+import { Image } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
+
+const fireIcon = require('../../assets/icon-fire.png')
+
+const locations = [{latitude: -33.8688,longitude: 151.2093}, {latitude: -33.86,longitude: 151.20}]
 
 export default function DisplayMap() {
   const [mapRegion, setmapRegion] = useState({
@@ -14,11 +19,23 @@ export default function DisplayMap() {
       style={{ alignSelf: 'stretch', height: '100%' }}
       region={mapRegion}
       showsScale={true}
+      rotateEnabled={false}
+      showsPointsOfInterest={false}
+      showsBuildings={false}
+      // onMarkerPress={(e) => {console.log(e)}}
     >
-    <Marker coordinate = {{latitude: -33.8688,longitude: 151.2093}}
-     pinColor = {"purple"} // any color
-     title={"title"}
-     description={"description"}/>
-    </MapView> 
+      {locations.map((location) => 
+        <Marker 
+          coordinate = {location}
+          title={"Risk Level (0-5)"}
+          description={"Location"}
+        >
+          <Image 
+            source={fireIcon}
+            style={{height: 50, width: 50}}
+          />
+        </Marker>
+      )}
+      </MapView> 
   );
 }
